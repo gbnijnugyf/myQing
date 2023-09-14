8ad1b3e3c7d4a8efd527c8dbf79e7df166bf3cdd
gbnijnugyf <1612517397@qq.com>
2023-09-14 17:14:08 +0800

    pull and fix conflicts

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,42 +1,19 @@
-<<<<<<< HEAD
-/* eslint-disable jsx-quotes */
 import { View, Text, Button } from "@tarojs/components";
-import { BrowserRouter } from "react-router-dom";
-import { useLoad } from "@tarojs/taro";
-import { Routers } from "@/router";
-import { useState } from "react";
-=======
-import { View, Text } from "@tarojs/components";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import { useLoad } from "@tarojs/taro";
-import { Button } from 'antd-mobile/2x'
->>>>>>> a1a98775b97fe201fca717dc1da6d03b300f3a0b
 import "./index.scss";
 
 export default function Index() {
   useLoad(() => {
     console.log("Page loaded.");
   });
-<<<<<<< HEAD
-  const[state, setState] = useState<boolean>(false)
-  return (
-    <>
-      <View className="index">
-        <Button plain type="primary" size="mini" onClick={()=>setState(true)}>
-          点击进入
-        </Button>
-        <Text>Hello</Text>
-      </View>
-      <BrowserRouter>
-        <Routers state={state} />
-=======
 
   return (
     <>
       <View className='index'>
         <Text>Hello world!</Text>
       </View>
-      <Button size='large'>das</Button>
+      <Button>das</Button>
       <BrowserRouter>
         <View className='drawer-box'>
           <View className='box-item'>
@@ -56,13 +33,10 @@ export default function Index() {
           <Route path='/pages/router/index/view2' element={<View2 />}></Route>
           <Route path='/pages/router/index/:id' element={<View3 />}></Route>
         </Routes>
->>>>>>> a1a98775b97fe201fca717dc1da6d03b300f3a0b
       </BrowserRouter>
     </>
   );
 }
-<<<<<<< HEAD
-=======
 
 function Home(){
   return(<></>)
@@ -74,4 +48,3 @@ function View1(){
 }function View3(){
   return(<></>)
 }
->>>>>>> a1a98775b97fe201fca717dc1da6d03b300f3a0b
